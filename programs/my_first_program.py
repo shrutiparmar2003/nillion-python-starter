cf9dabc5ceb91e1e9023cac6dcdea03f792ebab3
shrutiparmar2003 <shrutiparmar01082003@gmail.com>
2024-06-28 05:28:16 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -5,8 +5,24 @@ def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    my_int3 = SecretInteger(Input(name="my_int3", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Calculate the average of the three input integers
+    sum_ints = my_int1 + my_int2 + my_int3
+    average = sum_ints / 3
 
-    return [Output(my_int1, "my_output", party1)]
+    # Square the average
+    squared_average = average * average
+
+    # Compute the difference between the square of average and each original integer squared
+    diff1 = squared_average - (my_int1 * my_int1)
+    diff2 = squared_average - (my_int2 * my_int2)
+    diff3 = squared_average - (my_int3 * my_int3)
+
+    # Outputs
+    return [
+        Output(squared_average, "squared_average_output", party1),
+        Output(diff1, "diff1_output", party1),
+        Output(diff2, "diff2_output", party1),
+        Output(diff3, "diff3_output", party1)
+    ]
